81f8f1fb0fb0adfa95e865c128468e2941adca34
syednabeel527 <syednabeelsu2005@gmail.com>
2026-06-24 19:43:11 +0530

    Basic in Strings

---
+++ Strings.java
@@ -0,0 +1,8 @@
+public class Strings{
+    public static void main(String [] args){
+        System.out.println("Hello world");
+        System.out.println("Hello World 1" + "Hello Java Community");
+        System.out.println("5+8");
+        System.out.println(5+8);
+    }
+}
