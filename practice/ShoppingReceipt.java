ad851dc62c44b20882acee6c39a06930b9d251d1
syednabeel527 <syednabeelsu2005@gmail.com>
2026-07-02 17:43:36 +0530

    more practice on datatypes topics

---
+++ practice/ShoppingReceipt.java
@@ -0,0 +1,16 @@
+public class ShoppingReceipt {
+    public static void main (String [] args ){
+        String customerName = "Syed Nabeel ";
+        System.out.println("Customer Name : " + customerName);
+        String productName = "RAM" ;
+        System.out.println("Product Name : " + productName );
+        int productId = 5555 ;
+        System.out.println("Product ID : "+ productId );
+        int productQuantity = 2 ;
+        System.out.println("Product Quantity : "+ productQuantity);
+        boolean productDiscount = true ;
+        System.out.println("Product Discount : " +productDiscount );
+        float sellerRating = 4.3f ;
+        System.out.println("Seller Rating :" +sellerRating );
+    }
+}
---
+++ practice/StudentIDCard.java
@@ -0,0 +1,20 @@
+public class StudentIDCard {
+    public static void main (String [] args) {
+        int studentId = 05 ;
+        System.out.println("Student ID : " +studentId );
+        String studentName = "Syed Nabeel";
+        System.out.println("Student Name : " +studentName);
+        String collegeName = "CIT" ;
+        System.out.println("College name : " +collegeName );
+        String branch = "CSE" ;
+        System.out.println("Banch : " +branch);
+        int semester = 7;
+        System.out.println("semester : " + semester  );
+        int CGPA = 8 ;
+        System.out.println("CGPA : " +CGPA);
+        char section = 'B';
+        System.out.println ("Section : " + section);
+        boolean hostelStudent = false;
+        System.out.println("Hostel student : " + hostelStudent );
+        }
+}
